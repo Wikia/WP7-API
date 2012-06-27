f2945efea02148a940ff5941e991147c1da4e4ba
Hazardius <hazardiusam@gmail.com>
2012-06-27 12:52:42 +0200

    Licence note

--- Example1/Example1/Site.cs
+++ Example1/Example1/Site.cs
@@ -1,15 +1,29 @@
 ﻿// DotNetMetroWikiaAPI - Wikia API for C# Metro Style Applications
-// Distributed under the terms of the license:
-// TODO : NAME THE USED LICENCE
+//
 // Authors:
 // Hazardius (2012-*) hazardiusam at gmail dot com
 //
-// Work based o:n
+// Work based on:
 // DotNetWikiBot Framework 2.101 - bot framework based on Microsoft .NET Framework 2.0 for wiki projects
 // Distributed under the terms of the MIT (X11) license: http://www.opensource.org/licenses/mit-license.php
 // Copyright (c) Iaroslav Vassiliev (2006-2012) codedriller@gmail.com
 //
-// Note: Most time it's just codedriller's code made to work on Windows Phone.
+// Distributed under the terms of the license:
+// This program is free software; you can redistribute it and/or modify
+// it under the terms of the GNU General Public License as published by
+// the Free Software Foundation; either version 2 of the License, or
+// (at your option) any later version.
+//
+// This program is distributed in the hope that it will be useful,
+// but WITHOUT ANY WARRANTY; without even the implied warranty of
+// MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the
+// GNU General Public License for more details.
+//
+// You should have received a copy of the GNU General Public License along
+// with this program; if not, write to the Free Software Foundation, Inc.,
+// 59 Temple Place - Suite 330, Boston, MA 02111-1307, USA.
+// http://www.gnu.org/copyleft/gpl.html
+
 
 using System;
 using System.Net;
@@ -859,7 +873,9 @@ namespace DotNetMetroWikiaAPI
             {
                 try
                 {
-                    respStr = await HTTPPost(pageURL);
+                    int qMarkIndex = pageURL.IndexOf('?');
+
+                    respStr = await HTTPPost(pageURL.Substring(qMarkIndex + 1);
 
                     //webResp = (HttpWebResponse)webReq.GetResponse();
                     break;
@@ -1006,7 +1022,7 @@ namespace DotNetMetroWikiaAPI
             string data = null;
 
             WebClient proxy = new WebClient();
-            data = await proxy.UploadStringTaskAsync(site, parameters);
+            data = await proxy.UploadStringTaskAsync(site + "/api.php", parameters);
 
             //WindowsPhonePostClient.PostClient proxy = new WindowsPhonePostClient.PostClient(parameters);
             //data = await proxy.DownloadStringTaskAsync(new Uri(site, UriKind.Absolute));
